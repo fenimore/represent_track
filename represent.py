e144902afb947d0497c5f1cf5e6e67be87009c88
polypmer <exorable.ludos@gmail.com>
2016-08-20 11:05:00 -0400

    get person and get person id

--- represent.py
+++ represent.py
@@ -1,18 +1,32 @@
-import requests
+import requests, json
 
-url_person = "https://www.govtrack.us/api/v2/person/"
+url_person_by_id = "https://www.govtrack.us/api/v2/person/"
+url_person = "https://www.govtrack.us/api/v2/person?q="
 
-warner = "412321"
-kaine = "412582"
 
-def get_person(person):
+warner_id = "412321"
+kaine_id = "412582"
+
+def get_person_by_id(person):
     """Get a Person profile by ID number """
-    url = url_person + person
-
+    url = url_person_by_id + person
     r = requests.get(url)
+    json_parsed = json.loads(r.text)
+    return json_parsed
 
-    return r.text
+def get_person(name):
+    """Get Person by name"""
+    url = url_person + name
+    r = requests.get(url)
+    # json parsed is a dict type
+    json_parsed = json.loads(r.text)
+    # In meta, total count means
+    # that there have been that many matches
+    # In 'kaines' case, that is only one
+    # Brian is 7...
+    return json_parsed
 
 
 if __name__ == "__main__":
-    print(get_person(warner))
+    kaine = get_person("brian")
+    print(kaine)

3e48e8d3d5e504ebb4e6450f0e1542e2a21a2144
polypmer <exorable.ludos@gmail.com>
2016-08-21 17:23:59 -0400

    I dont know if this is feasible...

--- represent.py
+++ represent.py
@@ -3,16 +3,22 @@ import requests, json
 url_bill = "https://www.govtrack.us/api/v2/bill?"
 url_person_by_id = "https://www.govtrack.us/api/v2/person/"
 url_person = "https://www.govtrack.us/api/v2/person?q="
+url_votes = "https://www.govtrack.us/api/v2/vote_voter/?person="
 
 
 warner_id = "412321"
 kaine_id = "412582"
 
+def get_votes_by_person_id(person_id):
+    url = url_votes + person_id
+    j = requests.get(url).json()
+    return j
+
 def get_person_by_id(person):
     """Get a Person profile by ID number """
     url = url_person_by_id + person
     r = requests.get(url)
-    json_parsed = json.loads(r.text)
+    json_parsed = json.loads(r.text) # built in json
     return json_parsed
 
 def get_person(name):
@@ -31,8 +37,8 @@ def get_bill(branch, number, congress):
         &bill_type=senate_bill&congress=103
 
     Attributes:
-    branch - house or senate
-    number - bill number
+    branch - house or senate either senate_bill or house_bill
+    number - bill number id for govtrack
     congress - congress number
     """
     url = (url_bill + "bill_type=" + branch + "&number="
@@ -41,6 +47,20 @@ def get_bill(branch, number, congress):
     j = r.json()
     return j # maybe this should return the first objects[0]?
 
+def get_bill_title_by_id(bill_id):
+    url = "https://www.govtrack.us/api/v2/bill/" + bill_id
+    j = requests.get(url).json()  
+    return j['title']
+
+def test_votes():
+    v = get_votes_by_person_id("412582")
+    #print(type(v['objects'][0]))
+    return v['objects']
+    #for x in v['objects']:
+        #print(x)
+        #print(x[0]['value'])
+        #for x in v['objects'][0]:
+        #print(x['value']
 
 def test_bill():
     """Get bill takes branch, number, congress"""
@@ -68,4 +88,7 @@ def test_person():
 
         
 if __name__ == "__main__":
-    test_bill()
+    #test_bill()
+    test_votes()
+
+    
